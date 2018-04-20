b7faf6c6756a698a16bb789e197108f82bc8c317
Tiago Nunes <tiago.nunes@mindera.com>
2018-04-20 23:45:03 +0100

    Add code-splitting and async load

---
+++ src/components/LoadingError.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LoadingError = () => (
+  <div className="centered">Oops, could not load this page.</div>
+);
+
+export default LoadingError;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,21 @@
-import Home from './Home';
-import WeAreHuman from './WeAreHuman';
-import WeChange from './WeChange';
-import WeWorkTogether from './WeWorkTogether';
+import loadable from 'loadable-components';
 
-export { Home };
-export { WeAreHuman };
-export { WeChange };
-export { WeWorkTogether };
+import Loading from '../components/Loading';
+import LoadingError from '../components/LoadingError';
+
+export const Home = loadable(() => import(/* webpackChunkName: "home" */ './Home'), {
+  LoadingComponent: Loading,
+  ErrorComponent: LoadingError
+});
+export const WeAreHuman = loadable(() => import(/* webpackChunkName: "we-are-human" */ './WeAreHuman'), {
+  LoadingComponent: Loading,
+  ErrorComponent: LoadingError
+});
+export const WeChange = loadable(() => import(/* webpackChunkName: "we-change" */ './WeChange'), {
+  LoadingComponent: Loading,
+  ErrorComponent: LoadingError
+});
+export const WeWorkTogether = loadable(() => import(/* webpackChunkName: "we-work-together" */ './WeWorkTogether'), {
+  LoadingComponent: Loading,
+  ErrorComponent: LoadingError
+});
